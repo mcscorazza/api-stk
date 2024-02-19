1a93ac11a03b1053f0b26d858342e9c87aadf05e
mcscorazza <mcs.corazza@gmail.com>
2024-02-19 10:47:03 -0300

    .

--- routes/api.php
+++ routes/api.php
@@ -9,17 +9,6 @@ use App\Http\Controllers\SpedCodeController;
 use App\Http\Controllers\ProductTypeController;
 use App\Http\Controllers\ProductController;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
-|
-*/
-
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
